f29d31e1e72603c39b82ae1159cb2fd405948c0f
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-07 19:43:17 +0700

    commit1

---
+++ binarySearch.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silakan coba lagi.\n";
+        }
+    }
+    
+}
