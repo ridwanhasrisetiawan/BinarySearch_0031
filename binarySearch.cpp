ac29bd60b14960d96b122119ee19a128a35cd7b1
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-07 20:24:50 +0700

    commit5

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -77,6 +77,28 @@ void binarySearch()
         cout << "========================================\n";
         cout << "Masukkan elemen yang ingin dicari: ";
         cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int  mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[v] Elemen " << x << " ditemmukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mdi])
+            {
+                low 
+            }
+        }
+        
     }
     
 }
